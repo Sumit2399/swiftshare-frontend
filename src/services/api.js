dbf32a5ddf972acb614badda82b18a26f21fbba7
Sumit Dhyani <66456021+Sumit2399@users.noreply.github.com>
2025-03-23 19:24:59 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "swiftshare-backend-cydvavdbgkamdkg7.centralindia-01.azurewebsites.net"; // Replace with actual backend URL
+const API_BASE_URL = "https://swiftshare-backend-cydvavdbgkamdkg7.centralindia-01.azurewebsites.net"; // Replace with actual backend URL
 
 export const uploadContent = async (data) => {
   return await axios.post(`${API_BASE_URL}/upload`, data);
